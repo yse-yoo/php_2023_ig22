4f4d727a37a9a1869d84f4a86ccd775a52e7b8e4
yse-yoo <youhei_yoshikawa@yse-c.net>
2023-05-23 11:04:52 +0900

    add radio button

--- regist/input.php
+++ regist/input.php
@@ -23,6 +23,15 @@
             <label for="">Password</label>
             <input type="password" name="password">
         </div>
+        <div>
+            <label for="">Gender</label>
+
+            <input id="gender-male" type="radio" name="gender" value="male">
+            <label for="gender-male">Male</label>
+
+            <input id="gender-female" type="radio" name="gender" value="female">
+            <label for="gender-female">Female</label>
+        </div>
         <button>Confirm</button>
     </form>
 </body>
