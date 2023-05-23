e9c2854f87666c2f26018afdfb35a7cd8b976591
yse-yoo <youhei_yoshikawa@yse-c.net>
2023-05-23 10:25:34 +0900

    add form input

--- regist/input.php
+++ regist/input.php
@@ -1,12 +1,30 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Regist</title>
 </head>
+
 <body>
-   <h1>Regist</h1> 
+    <h1>Regist</h1>
+    <form action="" method="post">
+        <div>
+            <label for="">Name</label>
+            <input type="text" name="name">
+        </div>
+        <div>
+            <label for="">Email</label>
+            <input type="text" name="email">
+        </div>
+        <div>
+            <label for="">Password</label>
+            <input type="password" name="password">
+        </div>
+        <button>Confirm</button>
+    </form>
 </body>
+
 </html>
