6babb693307f02e3311ca389a5fb15798b567a7f
yse-yoo <youhei_yoshikawa@yse-c.net>
2023-05-30 09:56:18 +0900

    update

--- regist/input.php
+++ regist/input.php
@@ -10,53 +10,55 @@
 </head>
 
 <body>
-    <h1>Input</h1>
-    <form action="confirm.php" method="post">
-        <div>
-            <label for="">Name</label>
-            <input class="form-control" type="text" name="name">
-        </div>
-        <div>
-            <label for="">Email</label>
-            <input class="form-control" type="text" name="email">
-        </div>
-        <div>
-            <label for="">Password</label>
-            <input class="form-control" type="password" name="password">
-        </div>
-        <div>
-            <label for="">Gender</label>
+    <div class="container">
+        <h1>Input</h1>
+        <form action="confirm.php" method="post">
+            <div>
+                <label for="">Name</label>
+                <input class="form-control" type="text" name="name">
+            </div>
+            <div>
+                <label for="">Email</label>
+                <input class="form-control" type="text" name="email">
+            </div>
+            <div>
+                <label for="">Password</label>
+                <input class="form-control" type="password" name="password">
+            </div>
+            <div>
+                <label for="">Gender</label>
 
-            <input id="gender-male" type="radio" name="gender" value="male" checked>
-            <label for="gender-male">Male</label>
+                <input id="gender-male" type="radio" name="gender" value="male" checked>
+                <label for="gender-male">Male</label>
 
-            <input id="gender-female" type="radio" name="gender" value="female">
-            <label for="gender-female">Female</label>
-        </div>
-        <div>
-            <label for="">Birthday</label>
-            <select class="form-control" name="month" id="">
-                <option value="1">1</option>
-                <option value="2">2</option>
-                <option value="3">3</option>
-                <option value="4">4</option>
-                <option value="5">5</option>
-                <option value="6">6</option>
-                <option value="7">7</option>
-                <option value="8">8</option>
-                <option value="9">9</option>
-                <option value="10">10</option>
-                <option value="11">11</option>
-                <option value="12">12</option>
-            </select>
-            月
-        </div>
-        <div>
-            <label for="">Note</label>
-            <textarea class="form-control" name="note" id="" cols="80" rows="10"></textarea>
-        </div>
-        <button class="btn btn-primary">Confirm</button>
-    </form>
+                <input id="gender-female" type="radio" name="gender" value="female">
+                <label for="gender-female">Female</label>
+            </div>
+            <div>
+                <label for="">Birthday</label>
+                <select class="form-control" name="month" id="">
+                    <option value="1">1</option>
+                    <option value="2">2</option>
+                    <option value="3">3</option>
+                    <option value="4">4</option>
+                    <option value="5">5</option>
+                    <option value="6">6</option>
+                    <option value="7">7</option>
+                    <option value="8">8</option>
+                    <option value="9">9</option>
+                    <option value="10">10</option>
+                    <option value="11">11</option>
+                    <option value="12">12</option>
+                </select>
+                月
+            </div>
+            <div>
+                <label for="">Note</label>
+                <textarea class="form-control" name="note" id="" cols="80" rows="10"></textarea>
+            </div>
+            <button class="btn btn-primary">Confirm</button>
+        </form>
+    </div>
 </body>
 
 </html>
