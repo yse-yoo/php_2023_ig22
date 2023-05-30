0bd38657d7ae1f1a1bd64bc6de63d683d72fcbb9
yse-yoo <youhei_yoshikawa@yse-c.net>
2023-05-30 10:01:09 +0900

    update

--- regist/confirm.php
+++ regist/confirm.php
@@ -15,31 +15,33 @@ $regist = $_POST;
 </head>
 
 <body>
-    <h1>Confirm</h1>
-    <form action="" method="post">
-        <div>
-            <label for="">Name</label>
-            <p><?= $regist['name'] ?></p>
-        </div>
-        <div>
-            <label for="">Email</label>
-            <p><?= $regist['email'] ?></p>
-        </div>
-        <div>
-            <label for="">Gender</label>
-            <p><?= $regist['gender'] ?></p>
-        </div>
-        <div>
-            <label for="">Birthday</label>
-            <p><?= $regist['month'] ?></p>
-        </div>
-        <div>
-            <label for="">Note</label>
-            <p><?= nl2br($regist['note']) ?></p>
-        </div>
-        <button>Regist</button>
-        <a href="input.php">Back</a>
-    </form>
+    <div class="container">
+        <h1>Confirm</h1>
+        <form action="" method="post">
+            <div>
+                <label for="">Name</label>
+                <p><?= $regist['name'] ?></p>
+            </div>
+            <div>
+                <label for="">Email</label>
+                <p><?= $regist['email'] ?></p>
+            </div>
+            <div>
+                <label for="">Gender</label>
+                <p><?= $regist['gender'] ?></p>
+            </div>
+            <div>
+                <label for="">Birthday</label>
+                <p><?= $regist['month'] ?></p>
+            </div>
+            <div>
+                <label for="">Note</label>
+                <p><?= nl2br($regist['note']) ?></p>
+            </div>
+            <button>Regist</button>
+            <a href="input.php">Back</a>
+        </form>
+    </div>
 </body>
 
 </html>
